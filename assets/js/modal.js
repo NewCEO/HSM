e2285e327c142a9af7d1175116681eefda2ba29f
Cerebralhub <cerebralhubit@gmail.com>
2021-07-15 00:35:04 +0100

    pushing for show

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,7 +3,6 @@ var mpopup = document.getElementById('mpopupBox');
 
 // get the link that opens the mPopup
 var mpLink = document.getElementById("mpopupLink");
-
 // get the close action element
 var close = document.getElementsByClassName("close")[0];
 
@@ -24,11 +23,18 @@ window.onclick = function(event) {
     }
 };
 
+
+
+
+
+
 // get the mPopup
 var mpopup = document.getElementById('mpopupBox');
 
 // get the link that opens the mPopup
 var mpLink = document.getElementById("mpopupLink1");
+var mpLink = document.getElementById("mpopupLink2");
+var mpLink = document.getElementById("mpopupLink5");
 
 // get the close action element
 var close = document.getElementsByClassName("close")[0];
@@ -56,6 +62,64 @@ var mpopup = document.getElementById('mpopupBox');
 // get the link that opens the mPopup
 var mpLink = document.getElementById("mpopupLink2");
 
+// get the close action element
+var close = document.getElementsByClassName("close")[0];
+
+// open the mPopup once the link is clicked
+mpLink.onclick = function() {
+    mpopup.style.display = "block";
+}
+
+// close the mPopup once close element is clicked
+close.onclick = function() {
+    mpopup.style.display = "none";
+}
+
+// close the mPopup when user clicks outside of the box
+window.onclick = function(event) {
+    if (event.target == mpopup) {
+        mpopup.style.display = "none";
+    }
+};
+
+
+
+
+// get the mPopup
+var mpopup = document.getElementById('mpopupBox');
+
+// get the link that opens the mPopup
+var mpLink = document.getElementById("mpopupLink3");
+
+// get the close action element
+var close = document.getElementsByClassName("close")[0];
+
+// open the mPopup once the link is clicked
+mpLink.onclick = function() {
+    mpopup.style.display = "block";
+}
+
+// close the mPopup once close element is clicked
+close.onclick = function() {
+    mpopup.style.display = "none";
+}
+
+// close the mPopup when user clicks outside of the box
+window.onclick = function(event) {
+    if (event.target == mpopup) {
+        mpopup.style.display = "none";
+    }
+};
+
+
+
+// get the mPopup
+var mpopup = document.getElementById('mpopupBox');
+
+// get the link that opens the mPopup
+var mpLink = document.getElementById("mpopupLink9");
+var mpLink = document.getElementById("mpopupLink10");
+var mpLink = document.getElementById("mpopupLink11");
 // get the close action element
 var close = document.getElementsByClassName("close")[0];
 
